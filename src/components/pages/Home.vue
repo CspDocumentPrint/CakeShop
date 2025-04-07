54aa1b4bd6db729eea2fa9b2a69f37ec6d3cab9c
CspDocumentPrint <cspdocumentprint@gmail.com>
2025-04-07 16:39:04 +0530

    first commit

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1> Hallow World {{ name }}</h1>
+    </div>
+
+</template>
+<script>
+export default {
+    data() {
+        return {
+            name: "shruti"
+
+        }
+    }
+
+
+}
+</script>
