24108db95cccb28ffcddff06908eca887ff76084
CspDocumentPrint <cspdocumentprint@gmail.com>
2025-04-07 16:44:53 +0530

    image added

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1> Hallow World {{ name }}</h1>
+        <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" style="width: 150px ;"/>
     </div>
 
 </template>
